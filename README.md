c596cbc25905d32e35f714db6ac44bfb25580a4a
Griger <grigerratia@gmail.com>
2022-04-08 10:59:03 -0400

    First commit of React proyec second module

---
+++ README.md
@@ -0,0 +1,4 @@
+# curso-webpack-react
+
+#Esta parte del curso me gusto mas.
+    LA verdad el profe se pasa diciendo cosas raras
---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const App = () => <h1>Hello, this is my first React proyect. I'm excited</h1>
+
+export default App
